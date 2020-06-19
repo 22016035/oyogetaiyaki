a5505d7f244b3e034dd413605412c3eb0c43d63f
22016035 <22016035@hiroshima.kosen-ac.jp>
2020-06-19 14:14:39 +0900

    背景とたい焼きくんになる予定のブロックを配置しました。何もしないと重力によって落下しますがクリックによってジャンプします画面サイズの調整をしたい。

--- www/sketch.js
+++ www/sketch.js
@@ -1,11 +1,63 @@
+
+// ---- エンティティ関連の関数 ---------------------------------------------
+
+
+
+// ---- ゲーム全体に関わる部分 ---------------------------------------------
+
+/** プレイヤーエンティティ */
+let player;
+
 function setup() {
-  createCanvas(710, 400);
-  background(102);
+  createCanvas(800, 600);
+  rectMode(CENTER);
+
+  // プレイヤーを作成
+  taiyaki = createTaiyaki();
 }
 
 function draw() {
-  stroke(255);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
+  // たいやきの位置を更新
+  updatePosition(taiyaki);
+
+  // たいやきに重力を適用
+  applyGravity(taiyaki);
+
+  // たいやきを描画
+  background(0);
+  drawTaiyaki(taiyaki);
+}
+
+function mousePressed() {
+  // たいやきをジャンプさせる
+  applyJump(taiyaki);
+}
+// 全エンティティ共通
+
+function updatePosition(entity) {
+  entity.x += entity.vx;
+  entity.y += entity.vy;
+}
+
+// たいやきエンティティ
+
+function createTaiyaki() {
+  return {
+    x: 200,
+    y: 300,
+    vx: 0,
+    vy: 0
+  };
+}
+
+function applyGravity(entity) {
+  entity.vy += 0.15;
+}
+
+function applyJump(entity) {
+  entity.vy = -5;
+}
+
+function drawTaiyaki(entity) {
+  square(entity.x, entity.y, 40);
 }
